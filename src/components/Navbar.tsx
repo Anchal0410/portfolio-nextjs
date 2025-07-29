8a0b6e0f8d5df7312df7e72485b81c34223acc77
Anchal0410 <jainanchal0410@gmail.com>
2025-07-29 10:30:04 +0530

    fix: resume url update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,11 @@ const Navbar = () => {
               </div>
             </Tooltip>
           </Link>
-          <Link href="bit.ly/4kT23mX" target="_blank" underline="none">
+          <Link
+            href="https://drive.google.com/file/d/1ADam5FTNR-XqWrkrmC8ak7eSe24Vk9J3/view"
+            target="_blank"
+            underline="none"
+          >
             <Tooltip content="Resume">
               <div className="hover:px-3 max-sm:hover:px-2 py-2.5 dark:hover:bg-[#262626] hover:bg-[#F4F4F5] rounded-full transition-all duration-300">
                 <FileIcon className="w-[19px] h-[19px] max-sm:h-[15px] max-sm:w-[15px] text-black dark:text-white" />
